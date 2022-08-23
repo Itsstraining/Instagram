be4f2b6863020e85d124f0507b334c21637ec82e
Trong Pham <trong.phamtranduc@gmail.com>
2022-08-23 10:07:27 +0700

    auth

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import * as AuthAction from "src/app/actions/auth.action"
 import { Store } from '@ngrx/store';
 import { AuthState } from './states/auth.state';
+import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +10,17 @@ import { AuthState } from './states/auth.state';
 })
 export class AppComponent {
   title = 'client';
-  constructor(  private store:Store <{auth:AuthState}>){}
 
-  login(){
+  constructor(private store: Store<{ auth: AuthState }>, private AuthService: AuthService) {
+    this.AuthService.user$.subscribe(user => {
+      if (user.email) {
+        console.log(user);
+      }
+    })
+  }
+
+  login() {
     this.store.dispatch(AuthAction.login())
-    console.log("1");
+    // console.log("1");
   }
 }
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
-import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
-import { from } from 'rxjs';
+import { Auth, User } from '@angular/fire/auth';
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from '@firebase/auth';
+import { BehaviorSubject, from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth:Auth) { }
+  constructor(private auth: Auth) {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        this.user$.next(user);
+      }
+    })
+  }
 
-  login(){
-    return from(new Promise<string>(async(resolve,reject)=>{
-      try{
+  public user$ = new BehaviorSubject<User>(<User>{});
+
+  login() {
+    return from(new Promise<string>(async (resolve, reject) => {
+      try {
         let credential = await signInWithPopup(this.auth, new GoogleAuthProvider())
         let idToken = await credential.user.getIdToken();
-      resolve(idToken)
-      console.log(idToken) 
-    }catch{
-      reject("Cannot login with Google")
-    }
+        resolve(idToken)
+        console.log(idToken)
+      } catch {
+        reject("Cannot login with Google")
+      }
     }));
   }
 }
