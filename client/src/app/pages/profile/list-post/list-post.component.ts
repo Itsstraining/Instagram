2e71036e60e33c2fb4990e8e5843cdb1a5f8998d
Trong Pham <trong.phamtranduc@gmail.com>
2022-09-06 09:10:01 +0700

    detail fix

--- client/src/app/pages/profile/list-post/list-post.component.ts
+++ client/src/app/pages/profile/list-post/list-post.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-list-post',
@@ -7,12 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogService: NbDialogService) { }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading = true;
-    }, 3000)
+    }, 0)
   }
 
   isLoading = false;
@@ -31,5 +33,10 @@ export class ListPostComponent implements OnInit {
       image: "https://i.pinimg.com/564x/8c/ad/50/8cad504fae1fa6d4fd00d2d23923409e.jpg"
     }
   ]
+
+  detail() {
+    console.log("dialog");
+    // this.dialogService.open(dialog, { context: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' });
+  }
 
 }
